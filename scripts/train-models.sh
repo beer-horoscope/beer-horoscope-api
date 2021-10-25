81b9877d01c95f8b2e42a639590a2f46a4b5f071
kelee <kelee@localhost.localdomain>
2021-10-25 11:05:55 -0500

    updating train model scripts

--- scripts/train-models.sh
+++ scripts/train-models.sh
@@ -3,7 +3,7 @@
 # environment directories
 export TRAINED_MODELS_DIR=/mnt/storage/out/
 export VIRTENV_DIR=/home/temp/venv
-export REPOSITORY_DIR=/home/temp/beer-horoscope
+export REPOSITORY_DIR=/home/temp/beer-horoscope-api
 
 mkdir -p $TRAINED_MODELS_DIR
 virtualenv $VIRTENV_DIR
